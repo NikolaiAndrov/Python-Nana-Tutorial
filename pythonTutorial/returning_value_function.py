ae3f508c0e65ebbb91a88b4ed00ba1f29a6659e8
nikolaiandrov <nikolaiandrov@abv.bg>
2024-08-23 16:58:54 +0300

    check if input is digit

--- pythonTutorial/returning_value_function.py
+++ pythonTutorial/returning_value_function.py
@@ -1,12 +1,15 @@
-print("Enter a number of days.")
-days_input = int(input())
-
-
 def calculate_days_to_hours(days_count):
     if days_count > 0:
         days_to_hours = days_count * 24
         return f"{days_count} days are {days_to_hours} hours."
     return "Invalid input!"
 
-result = calculate_days_to_hours(days_input)
-print(result)
+
+user_input = input("Enter a number of days.\n")
+
+if user_input.isdigit():
+    days = int(user_input)
+    result = calculate_days_to_hours(days)
+    print(result)
+else:
+    print("Invalid input!")
