0052e8d38448261cfbde66b71d01c674a6c2b2dc
nikolaiandrov <nikolaiandrov@abv.bg>
2024-08-24 15:59:41 +0300

    add a list of days implemented

--- pythonTutorial/returning_value_function.py
+++ pythonTutorial/returning_value_function.py
@@ -2,9 +2,9 @@ def calculate_days_to_hours(days_count):
     days_to_hours = days_count * 24
     return f"{days_count} days are {days_to_hours} hours."
 
-def validate_and_execute(input_line):
+def validate_and_execute(days_count):
     try:
-        days = int(input_line)
+        days = int(days_count)
         if days <= 0:
             print("You should enter a valid positive number!")
         else:
@@ -18,4 +18,5 @@ while True:
     if user_input.lower() == "exit":
         break
 
-    validate_and_execute(user_input)
+    for current_day in user_input.split():
+        validate_and_execute(current_day)
