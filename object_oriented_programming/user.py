84cda54fa66972801995573271c10e62587575e9
nikolaiandrov <nikolaiandrov@abv.bg>
2024-08-28 23:20:16 +0300

    user class created

---
+++ object_oriented_programming/user.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, name, email, password, current_job_title):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.current_job_title = current_job_title
+
+    def change_password(self, new_password):
+        self.password = new_password
+
+    def change_job_title(self, new_job_title):
+        self.current_job_title = new_job_title
